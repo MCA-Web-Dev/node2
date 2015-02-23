0f4c84e84b3d2ae4421fa719931f47c3c956cd96
DrewWeth <dgwetherington@gmail.com>
2015-02-22 22:50:07 -0600

    first

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+  var arr = [
+    "https://pbs.twimg.com/profile_images/378800000532546226/dbe5f0727b69487016ffd67a6689e75a.jpeg",
+    "http://www.jeremynoeljohnson.com/wp-content/uploads/2014/06/crazy_cat_2.jpg",
+    "http://giveitlove.com/wp-content/uploads/Cat-Dressed-as-a-Bunny-For-Halloween.jpg"
+  ];
+
+  res.render('index', {
+    title: "Index",
+    stringArr: arr
+  });
+});
+
+app.get('/about', function(req, res){
+  res.render('about',{title:"About"})
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
